d68979e3566788f4bcb81d2583a1c10642d0b550
dawars <dawars00@gmail.com>
2017-05-20 23:14:23 +0200

    Added hogan templating engine

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 var express = require('express');
+var path = require('path');
 var connection = require("mysql");
 
 var app = express();
 
-app.use(express.static('static'));
-app.use('/anime/iisnode', express.static('iisnode'))
+// map static resources to url
+app.use('/anime/', express.static('static'));
+app.use('/anime/iisnode', express.static('iisnode')); // debug only
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+// set up template engine
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
 
+
+// url handling
 app.get('/anime/', function (req, res) {
-    res.send('Anime images!')
+
+    res.render('card', {
+        head_title: 'Image Labeling',
+        card_title: 'Introduction',
+        img: '/anime/img/aang.jpg',
+    });
 });
+
 app.get('/anime/tutorial', function (req, res) {
     res.send('Tutorial!')
 });
 
 app.get('/anime/getRandomImage', function (req, res) {
     var config = require('./db-config');
-    var mysql = require('mysql')
+    var mysql = require('mysql');
     var connection = mysql.createConnection(config);
 
-    connection.connect()
+    connection.connect();
 
     connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-        if (err) throw err
+        if (err) throw err;
 
         console.log('The solution is: ', rows[0].solution)
-    })
+    });
 
-    connection.end()
+    connection.end();
 
     res.send('end')
     /*
