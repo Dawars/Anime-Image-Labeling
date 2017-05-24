18bf09151967ee599ac02dd07a8fe19df38366cd
dawars <dawars00@gmail.com>
2017-05-24 19:20:05 +0200

    Back button, pop state kind of solved

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var config = require('./db-config');
 
 var app = express();
 
-
 // map static resources to url
 app.use('/anime/', express.static('static'));
 app.use('/anime/iisnode', express.static('iisnode')); // debug only
@@ -51,6 +50,7 @@ app.enable('trust proxy');
 var cardFunc = function (req, res) {
     // add new user
     var ip = req.headers['x-forwarded-for'];
+    console.log(ip);
     insertUser(ip);
 
     // init session
@@ -65,10 +65,15 @@ var cardFunc = function (req, res) {
             connection.connect();
             connection.query(sqlQueryLink, [imageId], function (err, rows, fields) {
                 if (err) throw err;
+                if (rows[0] === undefined) {
+                    res.status(404);
+                    return;
+                }
                 var imgLink = 'img/' + rows[0].series + '/' + rows[0].link;
                 res.render('card', {
                     head_title: 'Image Labeling',
                     card_title: 'Introduction',
+                    image_id: imageId,
                     img: imgLink
                 });
 
@@ -83,6 +88,7 @@ var cardFunc = function (req, res) {
         res.render('card', {
             head_title: 'Image Labeling',
             card_title: 'Introduction',
+            image_id: '-1',
             img: '/anime/img/aang.jpg'
         });
     }
@@ -148,7 +154,7 @@ app.post('/anime/next_image', function (req, res) {
         try {
             connection.connect();
             var query = connection.query(sqlInsertRating, vars, function (err, results, fields) {
-                if (err) throw err;
+                // if (err) throw err;
                 console.log('Inserted ', results.affectedRows, 'rows');
 
                 nextImageFunc(req, res, results.affectedRows > 0); // redirect to get impl
@@ -164,6 +170,49 @@ app.post('/anime/next_image', function (req, res) {
         nextImageFunc(req, res, true); // redirect to get impl
     }
 
+});
+
+const root = './static/img/';
+const fs = require('fs');
+
+const sqlInsertImages = 'INSERT INTO image (series, link, isanime) VALUES ?';
+function uploadDir(dir) {
+    fs.readdir(root + dir, function (err, files) {
+        // create array
+        var images = [];
+
+        files.forEach(function (file) {
+            var element = [dir, file, 1];
+            images.push(element);
+        });
+        var connection = mysql.createConnection(config);
+        try {
+            connection.connect();
+            var query = connection.query(sqlInsertImages, [images], function (err, results, fields) {
+                if (err) throw err;
+
+                console.log(dir, ' inserted with ', results.affectedRows, ' / ' + images.length);
+                // todo add check file when dir/file already done
+
+            });
+            console.log(query.sql);
+        } catch (e) {
+            console.error(e);
+        } finally {
+            connection.end();
+        }
+    });
+}
+app.get('/anime/update', function (req, res) {
+
+    fs.readdir(root, function (err, files) {
+        files.forEach(function (file) {
+            if (fs.statSync(root + file).isDirectory()) {
+                uploadDir(file);
+            }
+        });
+    });
+    res.send('done');
 });
 
 var port = process.env.PORT || 3000;
